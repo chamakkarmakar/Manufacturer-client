6adaa3617583b006af4181af024e9f441df7e549
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-05-25 15:22:44 +0600

    deleted product done

--- src/Pages/Dashboard/ManageProduct.js
+++ src/Pages/Dashboard/ManageProduct.js
@@ -4,13 +4,33 @@ const ManageProduct = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch('https://nameless-ocean-99245.herokuapp.com/product')
+        fetch('http://localhost:5000/product')
             .then(res => res.json())
             .then(data => {
                 // console.log(data);
                 setProducts(data);
             })
     }, [])
+
+    const handleProductDelete = id => {
+        const proceed = window.confirm('Are you sure to delete?');
+        if (proceed) {
+            const url = `http://localhost:5000/product/${id}`;
+            fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('userToken')}`
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = products.filter(product => product._id !== id);
+                    setProducts(remaining);
+                })
+        }
+    }
+    
     return (
         <div className="overflow-x-auto w-full">
             <table className="table w-full">
@@ -39,12 +59,11 @@ const ManageProduct = () => {
                                 </div>
                             </td>
                             <td>
-                               
                                 <span className="badge badge-ghost badge-sm px-5 py-3">{product.qty}</span>
                             </td>
                             <td>{product.price}</td>
                             <th>
-                                <button className="btn btn-ghost btn-xs">delete</button>
+                                <button onClick={()=>handleProductDelete(product._id)} className="btn btn-ghost btn-xs">delete</button>
                             </th>
                         </tr>
                             )
